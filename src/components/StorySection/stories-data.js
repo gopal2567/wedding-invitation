9b122ae5314538c133d59577e2125038984f38ee
gopal2567 <gopalgupta191995@gmail.com>
2024-01-22 00:58:06 +0530

    Add files via upload

---
+++ src/components/StorySection/stories-data.js
@@ -0,0 +1,32 @@
+import WeddingImg from '@assets/images/wedding.jpg';
+import HaldiImg from '@assets/images/haldi.jpg';
+import MehndiImg from '@assets/images/mehndi.png';
+import EngagmentImg from '@assets/images/engagement.jpg';
+import SangeetImg from '@assets/images/sangeet.jpg';
+
+export const stories = [
+  {
+    title: 'Mehndi Ceremony',
+    date: '22 Feb 2024, 4 pm',
+    description: 'Join us for this colorful tradition where mehndi adorns the hand and life takes on a new color.',
+    image: MehndiImg,
+  },
+  {
+    title: 'Haldi Ceremony',
+    date: '23 Feb 2024, 12 pm',
+    description: 'Get ready to be \'Haldified\' - because nothing says love like getting smothered in turmeric!',
+    image: HaldiImg,
+  },
+  {
+    title: 'The Wedding',
+    date: '24 Feb 2024, 7 pm',
+    description: 'The stage is set, the script is written – it\'s our wedding day! Join us as we say \'I do\' for the vows, the love, and the promise of forever.',
+    image: WeddingImg,
+  },
+  {
+    title: 'Reception',
+    date: '25 Feb 2024, 7 pm',
+    description: 'The party doesn\'t stop here! Your presence is requested at our reception to continue the wedding cheer!',
+    image: WeddingImg,
+  },
+];
